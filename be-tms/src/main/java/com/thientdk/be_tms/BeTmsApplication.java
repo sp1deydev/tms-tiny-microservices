59fa581a9d157172d9a40605e269555de1b5cbac
thientdk <duckhanhthientran@gmail.com>
2025-06-25 15:00:56 +0700

    <dependency>

--- be-tms/src/main/java/com/thientdk/be_tms/BeTmsApplication.java
+++ be-tms/src/main/java/com/thientdk/be_tms/BeTmsApplication.java
@@ -1,13 +1,22 @@
 package com.thientdk.be_tms;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeTmsApplication {
 
+	private static final Logger log = LogManager.getLogger(BeTmsApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BeTmsApplication.class, args);
+
+		log.info("========================================================");
+		log.info("|              TMS - TINY MICROSERVICE                 |");
+		log.info("|              BE TMS - EXPOSE API SERVICE             |");
+		log.info("========================================================");
 	}
 
 }
